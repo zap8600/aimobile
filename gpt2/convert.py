3c109b753936ffdbaa2a2ea909e72f2e8b65d667
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-03-20 21:13:10 +0000

    modified:   gpt2/convert.py 	modified:   gpt2/model.py

--- gpt2/convert.py
+++ gpt2/convert.py
@@ -3,7 +3,7 @@ from transformers import GPT2Config, GPT2Tokenizer, GPT2LMHeadModel
 
 enc = GPT2Tokenizer.from_pretrained('gpt2')
 
-text = "I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy."
+text = "I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy.I believe in rei plush supremacy."
 tokenized_text = enc.tokenize(text)
 
 indexed_tokens = enc.convert_tokens_to_ids(tokenized_text)
@@ -16,6 +16,8 @@ model = GPT2LMHeadModel(config)
 model.eval()
 
 traced_model = torch.jit.trace(model, [tokens_tensor])
+
+tokenizer.save_pretrained("./model")
 torch.jit.save(traced_model, "./model/traced_gpt2.pt")
 
 print("Done!")
--- gpt2/model.py
+++ gpt2/model.py
@@ -2,5 +2,13 @@ import torch
 from transformers import GPT2Tokenizer
 
 tokenizer = GPT2Tokenizer.from_pretrained("./model")
+tokenizer.pad_token = tokenizer.eos_token
 model = torch.jit.load("./model/traced_gpt2.pt")
 
+sequence = (input("Enter Prompt: "))
+
+inputs = tokenizer(sequence, padding='max_length', max_length=128)['input_ids']
+outputs = model(torch.tensor([inputs]))
+
+text = tokenizer.decode(outputs, skip_special_tokens=True)
+print(text)
