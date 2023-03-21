6b922970ea454164e14188a5b7c81748247cd0b1
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-03-21 15:27:02 +0000

    deleted:    gpt2/convert.py 	new file:   gpt2/download.py 	modified:   gpt2/model.py

--- gpt2/convert.py
+++
@@ -1,3 +0,0 @@
-from optimum.onnxruntime import ORTModelForCausalLM
-
-model = ORTModelForCausalLM.from_pretrained('gpt2', from_transformers=True)
--- gpt2/model.py
+++ gpt2/model.py
@@ -1,15 +1,2 @@
-import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-
-sequence = ([15006, 36934, 37516, 15006])
-
-print(len(sequence))
-print(sequence)
-
-inputs = torch.tensor(sequence)
-outputs = model.generate(inputs)
-
-tokenizer.decode(outputs)
+from transformers import GPT2Tokenizer
+from onnxruntime import InferenceSession
