9d72818f20f4176b0642a803f0d42893a99d8dea
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-03-22 22:06:04 -0500

    new file:   .gitignore 	modified:   gpt2/convert.py

--- gpt2/convert.py
+++ gpt2/convert.py
@@ -5,58 +5,40 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import coremltools as ct
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-sentence_fragment = "The Oceans are"
+sentence_fragment = "A compound sentence is"
 
-def top_p_filtering(logits, top_p=0.9, filter_value=-float('Inf')):
-    """ Implements nucleus (top-p) filtering """
+def top_p_filtering(logits, top_p=0.1, filter_value=-float('Inf')):
+    """Filter logits to only keep the top tokens with cumulative probability <= top_p."""
     sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-    cum_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+    cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
 
-    # Remove tokens with cumulative probability above the threshold
-    sorted_indices_to_remove = cum_probs > top_p
-    # Shift the indices to the right to keep the first token above the threshold
-    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-    sorted_indices_to_remove[..., 0] = 0
+    # Remove tokens with cumulative probability above the threshold.
+    sorted_indices_to_remove = cumulative_probs > top_p
+    if torch.any(sorted_indices_to_remove):
+        # Keep at least the top token that falls below the threshold.
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+        indices_to_remove = sorted_indices[sorted_indices_to_remove]
+        logits[:, indices_to_remove] = filter_value
 
-    # Fill the scores of the filtered-out tokens with a very small value
-    sorted_logits[sorted_indices_to_remove] = filter_value
-
-    # Restore the original order of the logits
-    indices = torch.argsort(sorted_indices)
-    logits = logits[indices]
     return logits
 
-class GPT2(torch.nn.Module):
-    def __init__(self, model):
-        super(GPT2, self).__init__()
-        self.next_token_predictor = model
-    
-    def forward(self, x):
-        inputs = x
-        inputs = self.next_token_predictor(inputs)
-
-token_predictor = GPT2LMHeadModel.from_pretrained("gpt2", torchscript=True).eval()
-
-context = torch.tensor(tokenizer.encode(sentence_fragment))
-random_tokens = torch.randint(10000, (512,))
-traced_token_predictor = torch.jit.trace(token_predictor, random_tokens)
-
-model = GPT2(model=traced_token_predictor)
-scripted_model = torch.jit.script(model)
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+compiled_model = torch.compile(model)
 
 # Custom model
 
-sentence_fragment = "The Oceans are"
+context = torch.tensor(tokenizer.encode(sentence_fragment))
 
-torch_out = scripted_model(context)
+torch_out = compiled_model(context)
 filtered_logits = top_p_filtering(torch_out[0][-1], top_p=0.9)
 
 probs = F.softmax(filtered_logits, dim=-1)
-next_token = torch.multinomial(probs, 1)
-
-generated_text = tokenizer.decode(torch.cat((input_ids[0], next_token)).tolist())
+next_token = torch.multinomial(probs, num_samples=1)
+generated_text = tokenizer.decode(torch.cat((context[0], next_token.squeeze())).tolist())
 print(generated_text)
 
+"""
 # Stock model
 
 model = GPT2LMHeadModel.from_pretrained("gpt2", torchscript=True).eval()
@@ -67,3 +49,4 @@ input_ids = tokenizer(sentence_fragment, return_tensors="pt").input_ids
 gen_tokens = model.generate(input_ids, do_sample=True, max_length=20)
 gen_text = tokenizer.batch_decode(gen_tokens)[0]
 print("Stock model: "+gen_text)
+"""
